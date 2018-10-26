03e3ec44e7a2964a0d296558c42cd477ca9adc0c
Taylor Vance <tmax8908@yahoo.com>
2018-10-26 11:46:08 -0400

    More links and WIP sign

--- project-list.js
+++ project-list.js
@@ -16,14 +16,17 @@ var PROJECTS = [
 	{
 		"title": "Mo Gold, Mo Dragons",
 		"description": "My entry for Ludum Dare 40. The theme was \"the more you have, the worse it is.\" I made the game, art, and music in 48 hours.",
-		"url": "http://htmlpreview.github.io/?https://github.com/taylorvance/fretboard/blob/master/index.html",
+		"url": "https://ldjam.com/events/ludum-dare/40/mo-gold-mo-dragons",
 		"img": "",
-		"externals": [{"name": "Unity3D", "url": "https://unity3d.com"}]
+		"externals": [
+			{"name": "Ludum Dare", "url": "https://ldjam.com"},
+			{"name": "Unity3D", "url": "https://unity3d.com"}
+		]
 	},
 	{
 		"title": "Umbra",
-		"description": "This is my first semi-full-featured game.",
-		"url": "",
+		"description": "This is my first semi-full-featured game. [download link coming soon]",
+		"url": "https://github.com/tvance989/umbra",
 		"img": "",
 		"externals": [{"name": "Unity3D", "url": "https://unity3d.com"}]
 	},
@@ -32,11 +35,12 @@ var PROJECTS = [
 		"description": "Steering behavior demo based on research by Craig Reynolds.",
 		"url": "/boids",
 		"img": "",
+		"externals": [{"name": "Boids algorithm", "url": "https://www.red3d.com/cwr/boids/"}]
 	},
 	{
 		"title": "Gear Font",
 		"description": "Type something and click start.",
-		"url": "",
+		"url": "https://jsfiddle.net/tvance989/ss03kqqp/",
 		"img": ""
 	}
 ]
