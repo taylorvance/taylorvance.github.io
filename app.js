2a24aa36c86639a9dbbce52bba17c95fb8ac0be5
Taylor Vance <tvance989@gmail.com>
2018-11-01 06:30:19 -0500

    Bandcamp embeds

--- app.js
+++ app.js
@@ -16,12 +16,48 @@ Vue.component('project-widget', {
 	</div>`
 });
 
+Vue.component('bandcamp-widget', {
+	props: ['album', 'description', 'url', 'albumId'],
+	computed: {
+		iframeSrc: function(){
+			return "https://bandcamp.com/EmbeddedPlayer/album=" + this.albumId + "/size=large/bgcol=ffffff/linkcol=0687f5/artwork=none/transparent=true/"
+		},
+	},
+	template: `<div class="bandcamp-widget">
+		<h3>{{ album }}</h3>
+		<p>{{ description }}</p>
+		<iframe style="border:0; width:100%; height:350px;" :src="iframeSrc" seamless>
+			<a :href="url">{{ album }}</a>
+		</iframe>
+	</div>`
+});
+
 
 new Vue({
 	el: '#app',
 	data: function() {
 		return {
 			projects: PROJECTS,
+			albums: [
+				{
+					album: "The Dragon Emperor & His Largest Army",
+					description: "The Dragon Emperor & His Largest Army's self-titled album tells the story of the Dragon Emperor, a cosmic trickster whose goal is to conquer worlds and transform populations into mindless soldiers. It consists of ten vignettes, each telling a different story about the Dragon Emperor’s journey--and the devastation wrought on the planets in his path.",
+					url: "https://dragonemperor.bandcamp.com/album/the-dragon-emperor-his-largest-army",
+					albumId: "3282846906",
+				},
+				{
+					album: "CURI/OSITY",
+					description: "",
+					url: "https://dragonemperor.bandcamp.com/album/curi-osity",
+					albumId: "3599526398",
+				},
+				{
+					album: "HUMANe",
+					description: "HUMANe is something we conceived, wrote, and produced in one night in 11th grade.",
+					url: "https://theshynessclinic.bandcamp.com/album/humane",
+					albumId: "2714321413",
+				},
+			],
 		};
 	},
 });
